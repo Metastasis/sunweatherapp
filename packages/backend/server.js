98d4d841f20e3ce4e7812b7a31ec50634e1984da
ar.a.belyaev <ar.a.belyaev@tinkoff.ru>
2021-11-16 17:34:04 +0300

    auth

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -1,4 +1,10 @@
-const server = require('fastify')({ logger: true })
+const fastify = require('fastify');
+const server = fastify({ logger: true });
+const fastifyJWT = require('fastify-jwt');
+
+server.register(fastifyJWT, {
+  secret: 'superPasswordForArsb29'
+})
 
 server
   .listen(3001)
